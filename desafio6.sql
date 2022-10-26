52fedfee9a5ade3e308c872da04ccc8ad9acd980
Guidzs <guidz.2004@gmail.com>
2022-10-26 16:40:36 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT 
     FORMAT(MIN(P.value), 2) AS faturamento_minimo,
-    MAX(P.value) AS faturamento_maximo,
-    ROUND(AVG(P.value), 2) AS faturamento_medio,
-    ROUND(SUM(P.value), 2) AS faturamento_total
+    CONVERT(MAX(P.value), DECIMAL(5, 2)) AS faturamento_maximo,
+    CONVERT(AVG(P.value), DECIMAL(5, 2)) AS faturamento_medio,
+    CONVERT(SUM(P.value), DECIMAL(5, 2)) AS faturamento_total
 FROM
     SpotifyClone.users AS U
         INNER JOIN
